f01e56379a4e3abb61affc7d50ee4b38828199b5
FIRST_NAME LAST_NAME <MY_NAME@example.com>
2021-10-16 12:37:54 +0100

    A

--- app/src/main/java/com/sdenisov/sudoku/SudokuSolver.java
+++ app/src/main/java/com/sdenisov/sudoku/SudokuSolver.java
@@ -6,7 +6,6 @@ public class SudokuSolver {
     // Works by modifying sudokuData object so doesn't need to return a value.
     public static void solve(SudokuData sudokuData) {
         int index = 0; // From left to right, top to bottom
-        indexLoop:
         while (index < sudokuData.getRows() * sudokuData.getRows()) {
             SudokuData.SudokuCell currentCell = sudokuData.getValue(index / sudokuData.getRows(), // integer division
                     index % sudokuData.getRows());
@@ -21,7 +20,8 @@ public class SudokuSolver {
                 } // If a cell has reached maximum value and there are still errors then backtracking occurs
                 else {
                     currentCell.setValue(null); // First, the value of the cell is set to a blank
-                    while (true) {
+                    boolean moveBack = true;
+                    while (moveBack) {
                         index--; // Moves back by decrementing index
                         Log.d("project", String.valueOf(index));
                         if (index < 0) {
@@ -30,15 +30,12 @@ public class SudokuSolver {
                         SudokuData.SudokuCell currentCellMovingBack
                                 = sudokuData.getValue(index / sudokuData.getRows(),
                                 index % sudokuData.getRows()); // Finds new current cell for moving back
-                        if (!currentCellMovingBack.isInitialValue()) {
-                            if (currentCellMovingBack.getValue() == sudokuData.getRows()) {
-                                currentCellMovingBack.setValue(null); // Cell value set to empty
-                            } else {
-                                // If the current cell is below the maximum value and is not an initial value then it can
-                                // be incremented so moving back stops.
-                                currentCellMovingBack.setValue(currentCellMovingBack.getValue() + 1);
-                                continue indexLoop;
-                            }
+                        if (currentCellMovingBack.getValue() == sudokuData.getRows()) {
+                            currentCellMovingBack.setValue(null); // Cell value set to empty
+                        } else if (!currentCellMovingBack.isInitialValue()) {
+                            // If the current cell is below the maximum value and is not an initial value then it can
+                            // be incremented so moving back stops.
+                            moveBack = false;
                         }
                     }
                 }
