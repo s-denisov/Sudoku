aa0ad8fee674ade45fd7df4cbb98c7e824c387e2
FIRST_NAME LAST_NAME <MY_NAME@example.com>
2022-02-21 18:04:34 +0000

    Add PdfGeneratorActivity

---
+++ app/src/main/java/com/sdenisov/sudoku/PdfGeneratorActivity.java
@@ -0,0 +1,12 @@
+package com.sdenisov.sudoku;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class PdfGeneratorActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pdf_generator);
+    }
+}
